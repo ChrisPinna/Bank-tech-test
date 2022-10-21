a797bfd2d602bf0b6c8d9c4bcbffb5263e719b9c
christianpinna <christian.a.pinna@gmail.com>
2022-10-21 22:28:30 +0100

    Set up project and first feature test

---
+++ src/test/feature.test.js
@@ -0,0 +1,10 @@
+import BankFrontEnd from '../bankFrontEnd'
+
+describe('Bank Program', () => {
+    test('it should be able to make a deposit of 1000 and get a bank statement with transaction in it', () => {
+     const bank = new BankFrontEnd;
+     bank.deposit(1000);
+     expect(bank.printStatement())
+     .toBe('date || credit || debit || balance\n26/04/2022 || 1000.00 || || 1000.00');
+    });
+});
