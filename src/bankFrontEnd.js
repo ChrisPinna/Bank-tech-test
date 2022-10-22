3680b533229b95b8aa3f2d36186572cf10ffe228
christianpinna <christian.a.pinna@gmail.com>
2022-10-22 16:39:45 +0100

    Added deposit and printStatement methods to banckFrontEnd and hardcoded to pass test

--- src/bankFrontEnd.js
+++ src/bankFrontEnd.js
@@ -2,6 +2,12 @@ export default class BankFrontEnd {
     constructor() {
 
     };
-    
+
+    deposit = () => {
+
+    }   
+    printStatement = () => {
+        return 'date || credit || debit || balance\n26/04/2022 || 1000.00 || || 1000.00'
+    };
 
 };
