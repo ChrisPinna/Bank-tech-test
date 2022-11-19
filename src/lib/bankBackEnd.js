3957acb3aca05b68e28aa8902ab8b1a50ccf72cf
christianpinna <christian.a.pinna@gmail.com>
2022-11-19 13:00:15 +0000

    processTransaction method throws when argument is string

--- src/lib/bankBackEnd.js
+++ src/lib/bankBackEnd.js
@@ -6,6 +6,8 @@ export class BankBackEnd {
     console.log(Object.prototype.toString.call( req ))
     if(Object.prototype.toString.call( req ) === '[object Number]' ) {
       throw 'Argument is  a int!';
+    } else if (Object.prototype.toString.call( req ) === '[object String]') {
+      throw 'Argument is  a String!';
     }
   }
 };
--- src/test/unit/bankBackEnd.test.js
+++ src/test/unit/bankBackEnd.test.js
@@ -6,8 +6,12 @@ describe("processTransaction method", () => {
     const bank = new BankBackEnd;
     expect(()=>{bank.processTransaction({transactionType: 'deposit', amount: 1000})}).not.toThrow();
   });
-  it("should not accept an int as a argument", () => {
+  it("should not accept an number as a argument", () => {
     const bank = new BankBackEnd;
     expect(()=>{bank.processTransaction(1)}).toThrow();
+  });
+  it("should not accept an string as a argument", () => {
+    const bank = new BankBackEnd;
+    expect(()=>{bank.processTransaction('string')}).toThrow();
   });
 });
