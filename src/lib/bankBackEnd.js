842b9f03b0e9c88699f2b6faffa926c81dfbd1db
christianpinna <christian.a.pinna@gmail.com>
2022-11-19 13:05:42 +0000

    processTransaction method refactored to throw if argument is not an Object

--- src/lib/bankBackEnd.js
+++ src/lib/bankBackEnd.js
@@ -3,13 +3,8 @@ export class BankBackEnd {
     
   };
   processTransaction(req) {
-    console.log(Object.prototype.toString.call( req ))
-    if(Object.prototype.toString.call( req ) === '[object Number]' ) {
-      throw 'Argument is  a int!';
-    } else if (Object.prototype.toString.call( req ) === '[object String]') {
-      throw 'Argument is  a String!';
-    } else if (Object.prototype.toString.call( req ) === '[object Array]') {
-      throw 'Argument is  a Array!';
-    }
+    if(Object.prototype.toString.call( req ) !== '[object Object]' ) {
+      throw 'Argument is not an Object!';
+    } 
   }
 };
