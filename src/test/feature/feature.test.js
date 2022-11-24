6c4910ce783f92b0c3e624772791ef19bba4cbdd
christianpinna <christian.a.pinna@gmail.com>
2022-11-24 07:13:52 +0000

    Added feature test to get a statement with no transactions

--- src/test/feature/feature.test.js
+++ src/test/feature/feature.test.js
@@ -3,6 +3,13 @@ import { BankBackEnd } from "../../lib/bankBackEnd";
 jest.useFakeTimers().setSystemTime(new Date("2023-09-01"));
 
 describe("Bank Program", () => {
+  test("it should be able to get and empty statement", () => {
+    const bankBack = new BankBackEnd
+    const bankFront = new BankFrontEnd(bankBack);
+    expect(bankFront.printStatement()).toBe(
+      "date || credit || debit || balance"
+    );
+  });
   test("it should be able to make a deposit of 1000 and get a bank statement with transaction in it", () => {
     const bankBack = new BankBackEnd
     const bankFront = new BankFrontEnd(bankBack);
